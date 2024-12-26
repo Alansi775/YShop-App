d5365f7b9b00ce864c0d97e9be9bd0280a57b2f7
MOHAMMED SALEH <102057156+Alansi775@users.noreply.github.com>
2024-12-26 17:37:42 +0300

    Initial Commit

---
+++ YShop/YShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  YShopApp.swift
+//  YShop
+//
+//  Created by Mohammed on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct YShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YShopTests/YShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  YShopTests.swift
+//  YShopTests
+//
+//  Created by Mohammed on 26.12.2024.
+//
+
+import Testing
+@testable import YShop
+
+struct YShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
